8a66b851fac4601ec37ef2f9418787f1ed8a8e67
github username <github email address>
2019-06-26 18:10:41 -0400

    Done.

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,21 @@
+class DoctorsController < ApplicationController 
+
+    def new 
+        @doctor = Doctor.new
+    end
+
+    def index
+        @doctors = Doctor.all
+    end
+    
+    def show
+        @doctor = Doctor.find(params[:id])
+    end
+
+    private
+
+    def doctor_params(*args)
+        params.require(:doctor).permit(*args)
+    end
+
+end
